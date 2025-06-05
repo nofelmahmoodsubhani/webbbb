674f75e661139fb118cec1cdeeaaf10f869b5ae8
nofelmahmoodsubhani <134275540+nofelmahmoodsubhani@users.noreply.github.com>
2025-06-06 00:00:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+AOS.init({ duration: 1000, once: true });
+
+function toggleTheme() {
+  const html = document.documentElement;
+  const isDark = html.getAttribute('data-theme') === 'dark';
+  html.setAttribute('data-theme', isDark ? 'light' : 'dark');
+}
